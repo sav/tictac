f0b6c9b8443642436dc9a75d86ff962fdd55d04b
Savio Sena <savio.sena@gmail.com>
2026-07-10 15:32:22 -0300

    Add `plugins/skel.lua`

---
+++ plugins/skel.lua
@@ -0,0 +1,34 @@
+-- SPDX-License-Identifier: GPL-3.0-or-later
+-- Copyright (C) 2026 Savio Sena <savio.sena@gmail.com>
+--
+-- skel.lua — a tictac plugin skeleton
+--
+-- Usage:
+--   tictac --file db.pgn --plugin skel.lua
+
+local plugin = {}
+
+plugin.meta = {
+  name        = "skel",
+  version     = "1.0.0",
+  description = "tictac skeleton plugin",
+  args        = {},
+}
+
+function plugin.init(ctx)
+end
+
+function plugin.process(input, ctx)
+  return {
+    action = "abort",
+    game = input.game,
+    data = {},
+  }
+end
+
+function plugin.finish(ctx)
+end
+
+return plugin
+
+-- -- vim:ft=lua:ts=2:sw=2:
